760ddbf3dec91f269506c58ae46fd8488fe4a71f
FrL1902 <farrelrasyad.binus.ac.id>
2024-05-29 22:20:06 +0700

    register page done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employer;
 use App\Models\User;
+use App\Models\Worker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -17,6 +20,11 @@ class UserController extends Controller
     public function login_page()
     {
         return view('login');
+    }
+
+    public function register_page()
+    {
+        return view('register');
     }
 
     public function user_login(Request $request)
@@ -41,6 +49,56 @@ class UserController extends Controller
             session()->flash('status', 'informasi yang dimasukkan salah');
             return redirect()->back();
         }
+    }
+
+    public function make_account(Request $request)
+    {
+        // dd($request);
+        $request->validate([
+            'firstName' => 'required|min:2|max:50',
+            'lastName' => 'required|min:2|max:50',
+            'email' => 'required|unique:App\Models\User,email',
+            'password' => 'required'
+        ], [
+            'firstName.required' => 'nama depan harus diisi',
+            'firstName.min' => 'nama depan minimal 2 karakter',
+            'firstName.max' => 'nama depan maksimal 50 karakter',
+            'lastName.required' => 'nama belakang harus diisi',
+            'lastName.min' => 'nama belakang minimal 2 karakter',
+            'lastName.max' => 'nama belakang minimal 50 karakter',
+            'email.required' => 'email harus diisi',
+            'email.unique' => 'email telah terambil, masukkan email lain',
+            'password.required' => 'password harus diisi',
+        ]);
+
+        $account = new User();
+        $account->email = $request->email;
+        $account->password = Hash::make($request->password);
+        $account->role = 1; #default, langsung jadi worker dulu minimal, nanti daftar jadi workernya ntar aja
+        $account->first_name = $request->firstName;
+        $account->last_name = $request->lastName;
+        $account->phone_number = '-';
+        $account->city_id = '-';
+        $account->address = '-';
+        $account->account_activated = 'yes';
+        $account->image_path = 'https://cdn.discordapp.com/attachments/1211571942965125160/1245333634555314256/image.png?ex=66585ed3&is=66570d53&hm=c7dc8bd96a82f1acf657238514336414d8551bf3347fde69e6c3a94de8854cc6&';
+        $account->save();
+
+        $worker = new Worker();
+        $worker->user_email = $request->email;
+        $worker->worker_description = '-';
+        $worker->worker_preference = '-';
+        $worker->save();
+
+        $employer = new Employer();
+        $employer->is_unlocked = 'no';
+        $employer->user_email = $request->email;
+        $employer->employer_address = '-';
+        $employer->employer_description = '-';
+        $employer->save();
+
+
+        return redirect('/');
     }
 
     public function logout()
---
+++ resources/views/register.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OdderWork</title>
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/1211571942965125160/1244616830744530944/image.png?ex=6655c340&is=665471c0&hm=b64ac6949677b54e7e5954e58320f37e799a27d66076fab9329d62f0b1c34d0f&" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/tooplate-moso-interior.css" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <div class="w-50 center border rounded px-3 py-3 mx-auto">
+            <h2 class="text-center" style="color:white"><span style="color: rgb(57, 82, 208)">Odder</span><span style="color: rgb(245, 205, 25)">Work</span></h2>
+            <form action="/makeAccount" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="firstName" class="form-label">Nama Depan</label>
+                    <input type="text" name="firstName" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="lastName" class="form-label">Nama Belakang</label>
+                    <input type="text" name="lastName" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                @if ($errors->any())
+                        <p class="text-center text-danger">{{ $errors->first() }}</p>
+                @endif
+                <div class="form-group" style="padding-top:0; padding-bottom:0" id="submitIncomingButtonAdd">
+                    <div class="card mt-4">
+                        <button type="submit" class="btn btn-primary">Buat Akun</button>
+                    </div>
+                </div>
+            </form>
+            <a class="text-end" href="/loginPage" style>already have an account?</a>
+        </div>
+    </main>
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.backstretch.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
