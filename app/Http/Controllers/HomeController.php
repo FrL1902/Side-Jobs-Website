bdbe8e8fdb22038aa22d63f3c5b23aed0f80d0ef
FrL1902 <farrelrasyad.binus.ac.id>
2024-06-14 02:28:36 +0700

    home page done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $availableJobs = Job::where(function($query) {
+            $query->where('job_status', 'opened');
+        })
+        ->latest()->take(2)->get();
+
+        if(Auth::check()){
+            if(auth()->user()->role == 1){ //worker
+                $activeJobs = Job::where('worker_id', auth()->user()->id)
+                    ->where(function($query) {
+                        $query->where('job_status', 'opened')
+                            ->orWhere('job_status', 'ongoing');
+                    })
+                    ->latest()->take(2)->get();
+
+            } else if(auth()->user()->role == 2){ //employer
+                $activeJobs = Job::where('employer_id', auth()->user()->id)
+                    ->where(function($query) {
+                        $query->where('job_status', 'opened')
+                            ->orWhere('job_status', 'ongoing');
+                    })
+                    ->latest()->take(2)->get();
+            }
+
+            return view('home', compact('activeJobs','availableJobs'));
+        } else {
+            return view('home', compact('availableJobs'));
+        }
     }
 
     public function welcome(Request $request)
