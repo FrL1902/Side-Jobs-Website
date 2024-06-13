3e449985b3e3d817792080eef412ff65dcfd0cea
FrL1902 <farrelrasyad.binus.ac.id>
2024-06-14 02:29:29 +0700

    rating functions to see rating and comments

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+
+
+    public static function seeRating($id)
+    {
+        $rating = Rating::where('job_id', $id)->first();
+
+        return $rating->job_rating;
+    }
+
+    public static function seeComment($id)
+    {
+        $rating = Rating::where('job_id', $id)->first();
+
+        return $rating->job_comment;
+    }
 }
